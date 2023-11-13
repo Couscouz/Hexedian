67b3c820d7e1c72be6531fa7a7b67560ce537e44
couziremi <couziremi@cy-tech.fr>
2023-11-13 16:49:15 +0100

    testing parser

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 console.log("cou")
 
 file = "a"
+
+outputDir = "results"
+inputDir = "replays"
+
+command = "wotreplay-parser-master/bin/wotreplay-parser --parse --root wotreplay-parser-master --type gif --input replays/westfield.wotreplay --output results/test.gif"
---
+++ wotreplay-parser-master/src/version.cpp
@@ -0,0 +1,9 @@
+#include "version.h"
+
+using namespace wotreplay;
+
+const std::string Version::GIT_SHA1 = "ea5301c3648f6438abc41881cdd3c3a10eddf96a-dirty";
+const std::string Version::GIT_DATE = "Mon Nov 13 15:06:00 2023";
+const std::string Version::GIT_COMMIT_SUBJECT = "wot-replay-parser";
+const std::string Version::BUILD_DATE = "Mon Nov 13 15:39:34 2023";
+const std::string Version::BUILD_VERSION = "20231113.150600";
