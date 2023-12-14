d8bdacd7fd0354d7b846081adb2af13a7efc22c0
Couscouz <rem.couzi@gmail.com>
2023-12-14 11:17:26 +0100

    deported url

--- api/src/controllers/data.controller.js
+++ api/src/controllers/data.controller.js
@@ -1,8 +1,28 @@
 const cheerio = require('cheerio')
 const axios = require('axios')
 
+const getURL = (id,name) => {
+    return 'https://tomato.gg/stats/EU/' + name + '%3D'+ id;
+}
+
+const getOverallWN8 = async (id,name) => {
+    try {
+        const url = getURL(id,name)
+        axios.get().then(({ data }) => { 
+            const $ = cheerio.load(data); 
+     
+            const heatmapDiv = $('.fnejWB').eq(4)
+            console.log(heatmapDiv.text())
+            
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(400).jso
+    }
+}
+
 const perform = async (id,name) => {
-    const url = 'https://tomato.gg/stats/EU/' + name + '%3D'+ id;
+    const url =  getURL(id,name) + '    ?tab=advanced';
 
     // axios.get(url).then(({ data }) => { 
 	// 	const $ = cheerio.load(data); 
