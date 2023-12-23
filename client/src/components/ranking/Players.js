e1952133d780017aae4610ae515cd533d8e57a00
Couscouz <rem.couzi@gmail.com>
2023-12-23 15:05:14 +0100

    changed react port

--- client/src/components/ranking/Players.js
+++ client/src/components/ranking/Players.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import PlayerLine from "./PlayerLine";
 
-const Players = () => {
-  const players = [
-    {name: "Hexedian", recent: 5000, moe: 150},
-    {name: "Tomato", recent: 2000, moe: 20}
-  ]
+const API_URL = "localhost:3000/"
 
-  return (
-    <div>
-      <ul>
-        {players.map((player,index) => (
-          <PlayerLine key={index} player={player}/>
-        ))}
-      </ul>
-    </div>
-  );
+const Players = () => {
+    
+    const [players, setPlayers] = useState([]);
+
+    useEffect(() => {
+        axios.get(API_URL+"players/sort/recent/top/5")
+    },[]);
+    
+    const aplayers = [
+        {name: "Hexedian", recent: 5000, moe: 150},
+        {name: "Tomato", recent: 2000, moe: 20}
+    ]
+
+    return (
+        <div>
+        <ul>
+            {players.map((player,index) => (
+            <PlayerLine key={index} player={player}/>
+            ))}
+        </ul>
+        </div>
+    );
 };
 
 export default Players;
