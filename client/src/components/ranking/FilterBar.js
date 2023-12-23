d9ad9cf819602abd1a1bbbad163f101fa85ec1d6
Couscouz <rem.couzi@gmail.com>
2023-12-23 18:47:45 +0100

    filter bar template

---
+++ client/src/components/ranking/FilterBar.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+const FilterBar = () => {
+    return (
+        <div>
+        <ul>
+        </ul>
+        </div>
+    );
+};
+
+export default FilterBar;
